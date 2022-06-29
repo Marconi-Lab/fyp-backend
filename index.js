b430656d29f76b9edaeba4c7dbd03208fa1bd802
ben-wycliff <ben12wycliff@gmail.com>
2022-06-29 14:25:44 +0300

    Adds root endpoint

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const routes = require("./routes");
 
 const app = express();
 
-dotenv.config({path: './config/.env'});
+// ================================== ROUTES =======================================
+app.get("/", (req, res) => res.json({ msg: "FYP backend API" }));
+
+dotenv.config({ path: "./config/.env" });
 const port = process.env.PORT || 3000;
 
-module.exports = app.listen(port, ()=> console.log(`API running on port ${port}`));
+module.exports = app.listen(port, () =>
+  console.log(`API running at http://127.0.0.1:${port}`)
+);
