ca7e93284d1e7d98c72e4271055e61c6835c4343
AkolIvanCollins <akolivan13@gmail.com>
2022-09-28 17:56:12 +0300

    updated authetication

--- controllers/authetication.js
+++ controllers/authetication.js
@@ -45,3 +45,65 @@ exports.signup = async (req, res, next) => {
         console.log("Error detected");
     }
 }
+
+
+// -------------------------------------------------------login----------------------------------------------------
+exports.login = async (req, res, next) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        //check if email is exists
+        // if (!user) throw new Error("Email doesn't exist");
+        if (!user) return res.json({ message: "Email doesn't exist" });
+
+        //check if password is valid
+        const validPassword = await validatePassword(password, user.password);
+        // if (!validPassword) throw new Error("Password is not correct");
+        if (!validPassword) return res.json({ message: "Password is not correct" });
+
+        res.status(200).json({
+            data: { email: user.email, role: user.role },
+            message: "Login Successful",
+          });
+    }
+    catch(error){
+        res.status(401).json({
+            msg: `error message ${error}`,
+        });
+    }
+}
+
+exports.getUser = async (req, res, next) => {
+    try {
+      const userId = req.params.userId;
+      const user = await User.findById(userId);
+  
+      if (!user) return res.json({ message: "User doesnot exist" });
+      res.status(200).json({
+        data: user,
+      });
+    }
+    catch (error) {
+    //   next(error);
+      res.status(401).json({
+        msg: `ID incorrect`,
+    });
+    }
+};
+
+exports.allowIfLoggedin = async (req, res, next) => {
+    try {
+      const user = res.locals.loggedInUser;
+      if (!user) {
+        return res.status(401).json({
+          error: "You need to be logged in to access this route",
+        });
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+  
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const { allowIfLoggedin } = require('../controllers/');
 const router = express.Router();
 const authn = require("../controllers/");
+const token = require('../controllers/auth')
 
 router.post("/signup", authn.signup);
+
+router.post("/login", authn.login);
+
+// router.get("/user/:userId", authn.getUser);
+
+router.get("/user/:userId", token.auth , authn.allowIfLoggedin, authn.getUser)
 
 module.exports = router;
